5cc9ac4c2448138c0b5c3359f7fc89b5bac0c0ab
Stephanie Asmar <sasmar8@gmail.com>
2017-07-22 15:34:51 -0400

    Defines global variables for text value data attributes

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,9 @@
 //(For example, if you keep typing the same selector string over and over,
 //that's a good sign that you should create a global variable.)
 
+//GLOBAL VARIABLES HERE:
+COFFEE_SELECTOR = '[data-coffee-order="coffeeOrder"]'
+EMAIL_SELECTOR = '[data-email-input="email"]'
 
 
 
@@ -16,23 +19,40 @@ function storeUserInput(key, value) {
 }
 
 
-//1. When the user submiits, get data from form
+//1.a When the user submits in a Text Area, get data from form:
 
-function getUserInput(textArea) {
+function getUserTextInput(textArea) {
     var $input = $(textArea); //defines variable for the form filed where user inputs their coffee order
-    var key = $input.attr(['name']); //defines variable for the key names
-    var value = $input.val(); //defines variable for key values
+    var $key = $input.attr('name'); //defines variable for the key names
+    var $value = $input.val(); //defines variable for key values
     storeUserInput(key, value); //calls a a previously devined function that will wtore the user's input to local storage
 }
+
+//1b. When the user submits in a Dropdown Menu, get data from form:
+
+
+//1c. When the user submits in a Radio Button, get data from form:
 
 
 //3. When the user refreshes the browser, repopulate the form with the data
 
+function populateInputOnRefresh() {
+  
+}
 
 
 
 
 
 
-//4. Main function
+//4. Main function: all the things will be done/executed by main
 
+function main() {
+    form.on('submit', function(event) {
+            getUserTextInput(COFFEE_SELECTOR);
+            getUserTextInput(EMAIL_SELECTOR);
+
+    });
+}
+
+main();
